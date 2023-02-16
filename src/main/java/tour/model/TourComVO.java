7baaa2b3a1b66776155375f91f3496fb83cc1766
YiFan <yifanchen0914@gmail.com>
2023-02-16 17:47:23 +0800

    廠商上架行程的VO新增annotation

--- src/main/java/tour/model/TourComVO.java
+++ src/main/java/tour/model/TourComVO.java
@@ -2,119 +2,115 @@ package tour.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="tour_company")
 public class TourComVO implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name = "c_tour_id")
+	private Integer tourComId;
+	@Column(name="company_id")
+	private String tourTitle;
+	@Column(name="tour_title")
+	private String startDate;
+	@Column(name="start_date")
+	private String endDate;
+	@Column(name="end_date")
+	private String tourImg;
+	@Column(name="cost")
+	private Integer cost;
+	@Column(name="tour_person")
+	private Integer tourPerson;
+	@Column(name="company_id")
+	private Integer companyId;
+	private byte[] tourImgByte;
+	
 
-    private Integer tourComId;
-    private String tourTitle;
-    private String startDate;
-    private String endDate;
-    private String tourImg;
-    private Integer cost;
-    private Integer tourPerson;
-    private Integer companyId;
-    private byte[] tourImgByte;
-
-
-    public TourComVO() {
-    }
-
-    public TourComVO(Integer tourComId, String tourTitle, String startDate, String endDate, String tourImg,
-                     Integer cost, Integer tourPerson, Integer companyId, byte[] tourImgByte) {
-        this.tourComId = tourComId;
-        this.tourTitle = tourTitle;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.tourImg = tourImg;
-        this.cost = cost;
-        this.tourPerson = tourPerson;
-        this.companyId = companyId;
-        this.tourImgByte = tourImgByte;
-    }
-
-    public Integer getTourComId() {
-        return tourComId;
-    }
-
-    public void setTourComId(Integer tourComId) {
-        this.tourComId = tourComId;
-    }
-
-    public String getTourTitle() {
-        return tourTitle;
-    }
-
-    public void setTourTitle(String tourTitle) {
-        this.tourTitle = tourTitle;
-    }
-
-    public String getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getTourImg() {
-        return tourImg;
-    }
-
-    public void setTourImg(String tourImg) {
-        this.tourImg = tourImg;
-    }
-
-    public Integer getCost() {
-        return cost;
-    }
-
-    public void setCost(Integer cost) {
-        this.cost = cost;
-    }
-
-    public Integer getTourPerson() {
-        return tourPerson;
-    }
-
-    public void setTourPerson(Integer tourPerson) {
-        this.tourPerson = tourPerson;
-    }
-
-    public Integer getCompanyId() {
-        return companyId;
-    }
-
-    public void setCompanyId(Integer companyId) {
-        this.companyId = companyId;
-    }
-
-    public static long getSerialversionuid() {
-        return serialVersionUID;
-    }
-
-    public byte[] getTourImgByte() {
-        return tourImgByte;
-    }
-
-    public void setTourImgByte(byte[] tourImgByte) {
-        this.tourImgByte = tourImgByte;
-    }
-
-    @Override
-    public String toString() {
-        return "TourComVO [tourComId=" + tourComId + ", tourTitle=" + tourTitle + ", startDate=" + startDate
-                + ", endDate=" + endDate + ", tourImg=" + tourImg + ", cost=" + cost + ", tourPerson=" + tourPerson
-                + ", companyId=" + companyId + "]";
-    }
+	public TourComVO() {
+	}
+	
+	public TourComVO(Integer tourComId, String tourTitle, String startDate, String endDate, String tourImg,
+			Integer cost, Integer tourPerson, Integer companyId, byte[] tourImgByte) {
+		this.tourComId = tourComId;
+		this.tourTitle = tourTitle;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.tourImg = tourImg;
+		this.cost = cost;
+		this.tourPerson = tourPerson;
+		this.companyId = companyId;
+		this.tourImgByte= tourImgByte;
+	}
+	
+	public Integer getTourComId() {
+		return tourComId;
+	}
+	public void setTourComId(Integer tourComId) {
+		this.tourComId = tourComId;
+	}
+	public String getTourTitle() {
+		return tourTitle;
+	}
+	public void setTourTitle(String tourTitle) {
+		this.tourTitle = tourTitle;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getTourImg() {
+		return tourImg;
+	}
+	public void setTourImg(String tourImg) {
+		this.tourImg = tourImg;
+	}
+	public Integer getCost() {
+		return cost;
+	}
+	public void setCost(Integer cost) {
+		this.cost = cost;
+	}
+	public Integer getTourPerson() {
+		return tourPerson;
+	}
+	public void setTourPerson(Integer tourPerson) {
+		this.tourPerson = tourPerson;
+	}
+	public Integer getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(Integer companyId) {
+		this.companyId = companyId;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public byte[] getTourImgByte() {
+		return tourImgByte;
+	}
+	public void setTourImgByte(byte[] tourImgByte) {
+		this.tourImgByte = tourImgByte;
+	}
+	
+	@Override
+	public String toString() {
+		return "TourComVO [tourComId=" + tourComId + ", tourTitle=" + tourTitle + ", startDate=" + startDate
+				+ ", endDate=" + endDate + ", tourImg=" + tourImg + ", cost=" + cost + ", tourPerson=" + tourPerson
+				+ ", companyId=" + companyId + "]";
+	}
 
 
 }
--- src/main/java/tour/model/TourScheduleComVO.java
+++ src/main/java/tour/model/TourScheduleComVO.java
@@ -2,83 +2,84 @@ package tour.model;
 
 import java.io.Serializable;
 
-public class TourScheduleComVO implements Serializable {
-    private static final long serialVersionUID = 1L;
-
-    private Integer tourScheduleComId;
-    private Integer tourComId;
-    private Integer attractionId;
-    private String date;
-    private String startTime;
-    private String stayTime;
-
-    public TourScheduleComVO() {
-    }
-
-    public Integer getTourScheduleComId() {
-        return tourScheduleComId;
-    }
-
-    public void setTourScheduleComId(Integer tourScheduleComId) {
-        this.tourScheduleComId = tourScheduleComId;
-    }
-
-    public Integer getTourComId() {
-        return tourComId;
-    }
-
-    public void setTourComId(Integer tourComId) {
-        this.tourComId = tourComId;
-    }
-
-    public Integer getAttractionId() {
-        return attractionId;
-    }
-
-    public void setAttractionId(Integer attractionId) {
-        this.attractionId = attractionId;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
-
-    public String getStayTime() {
-        return stayTime;
-    }
-
-    public void setStayTime(String stayTime) {
-        this.stayTime = stayTime;
-    }
-
-    public TourScheduleComVO(Integer tourScheduleComId, Integer tourComId, Integer attractionId, String date,
-                             String startTime, String stayTime) {
-        super();
-        this.tourScheduleComId = tourScheduleComId;
-        this.tourComId = tourComId;
-        this.attractionId = attractionId;
-        this.date = date;
-        this.startTime = startTime;
-        this.stayTime = stayTime;
-    }
-
-    @Override
-    public String toString() {
-        return "TourScheduleComVO [tourScheduleComId=" + tourScheduleComId + ", tourComId=" + tourComId
-                + ", attractionId=" + attractionId + ", date=" + date + ", startTime=" + startTime + ", stayTime="
-                + stayTime + "]";
-    }
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="tour_schedule_company")
+public class TourScheduleComVO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name="c_tour_schedule_id")
+	private Integer tourScheduleComId;
+	@Column(name="c_tour_id")
+	private Integer tourComId;
+	@Column(name="attraction_id")
+	private Integer attractionId;
+	@Column(name="date")
+	private String date;
+	@Column(name="start_time")
+	private String startTime;
+	@Column(name="stay_time")
+	private String stayTime;
+	
+	public TourScheduleComVO() {
+	}
+	
+	public Integer getTourScheduleComId() {
+		return tourScheduleComId;
+	}
+	public void setTourScheduleComId(Integer tourScheduleComId) {
+		this.tourScheduleComId = tourScheduleComId;
+	}
+	public Integer getTourComId() {
+		return tourComId;
+	}
+	public void setTourComId(Integer tourComId) {
+		this.tourComId = tourComId;
+	}
+	public Integer getAttractionId() {
+		return attractionId;
+	}
+	public void setAttractionId(Integer attractionId) {
+		this.attractionId = attractionId;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getStayTime() {
+		return stayTime;
+	}
+	public void setStayTime(String stayTime) {
+		this.stayTime = stayTime;
+	}
+	
+	public TourScheduleComVO(Integer tourScheduleComId, Integer tourComId, Integer attractionId, String date,
+			String startTime, String stayTime) {
+		super();
+		this.tourScheduleComId = tourScheduleComId;
+		this.tourComId = tourComId;
+		this.attractionId = attractionId;
+		this.date = date;
+		this.startTime = startTime;
+		this.stayTime = stayTime;
+	}
+	
+	@Override
+	public String toString() {
+		return "TourScheduleComVO [tourScheduleComId=" + tourScheduleComId + ", tourComId=" + tourComId
+				+ ", attractionId=" + attractionId + ", date=" + date + ", startTime=" + startTime + ", stayTime="
+				+ stayTime + "]";
+	}
 
 }
