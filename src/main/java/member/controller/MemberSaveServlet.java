d2eb76a2112800d66b1949fc73aa9e9eb0bfbcbc
Henry <g23821208@gmail.com>
2023-02-24 00:17:04 +0800

    member修改個人資料

--- src/main/java/member/controller/MemberSaveServlet.java
+++ src/main/java/member/controller/MemberSaveServlet.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,6 +23,7 @@ import member.service.MemberServiceImpl;
 public class MemberSaveServlet extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
 		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
 		Member member = gson.fromJson(req.getReader(), Member.class);
 		try {
@@ -34,6 +36,18 @@ public class MemberSaveServlet extends HttpServlet{
 				String resultStr = service.save(member);
 				jsonObj.addProperty("successful", resultStr.equals("修改成功"));
 				jsonObj.addProperty("message", resultStr);
+				
+				String username = "memUsername";
+				Cookie[] cookie = req.getCookies();
+				for (Cookie c: cookie) {
+//					System.out.println(c.getName());
+			        if (username.equals(c.getName())) {
+			        	c.setValue(member.getUsername());
+			        	c.setPath("/");
+			            resp.addCookie(c);
+			        }
+			    }
+				
 			}
 			resp.getWriter().append(jsonObj.toString());
 		} catch (NamingException e) {
