6aced87ab6593924a4b0e51907cd19ef459e91b8
Henry <g23821208@gmail.com>
2023-03-16 14:08:01 +0800

    feat: 登入頁面可以擋住停權的用戶

--- src/main/java/member/controller/MemberLoginServlet.java
+++ src/main/java/member/controller/MemberLoginServlet.java
@@ -39,38 +39,42 @@ public class MemberLoginServlet extends HttpServlet{
 			    resp.setCharacterEncoding("UTF-8");
 			    resp.getWriter().write(error.toString());
 			} else {
-				if (req.getSession(false) != null) {
-					req.changeSessionId(); // ←產生新的Session ID
-				}
-				
-				req.getSession().setAttribute("member", member);
-				req.getSession().setAttribute("login", true);
-				
-				Cookie cookie = new Cookie("memId", member.getId().toString());
-				Cookie cookie2 = new Cookie("memUsername", member.getUsername());
-				cookie.setMaxAge(30 * 60);
-				cookie2.setMaxAge(30 * 60);
-				cookie.setPath("/");
-				cookie2.setPath("/");
-				resp.addCookie(cookie);
-				resp.addCookie(cookie2);
-				
-				String location = (String) req.getSession().getAttribute("location");
-				if(location == null || location.isEmpty()) {
-					resp.sendRedirect(req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort()  + req.getContextPath() + "/" + "index.html");
+				if(member.getAccessnum().equals("0")) {
+					JsonObject error = new JsonObject();
+				    error.addProperty("errorMessage", "此用戶已被停權，請聯繫客服");
+				    resp.setContentType("application/json");
+				    resp.setCharacterEncoding("UTF-8");
+				    resp.getWriter().write(error.toString());
 				}else {
-					resp.sendRedirect(location);
-				}
+					if (req.getSession(false) != null) {
+						req.changeSessionId(); // ←產生新的Session ID
+					}
+					
+					req.getSession().setAttribute("member", member);
+					req.getSession().setAttribute("login", true);
+					
+					Cookie cookie = new Cookie("memId", member.getId().toString());
+					Cookie cookie2 = new Cookie("memUsername", member.getUsername());
+					cookie.setMaxAge(30 * 60);
+					cookie2.setMaxAge(30 * 60);
+					cookie.setPath("/");
+					cookie2.setPath("/");
+					resp.addCookie(cookie);
+					resp.addCookie(cookie2);
+					
+					String location = (String) req.getSession().getAttribute("location");
+					if(location == null || location.isEmpty()) {
+						resp.sendRedirect(req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort()  + req.getContextPath() + "/" + "index.html");
+					}else {
+						resp.sendRedirect(location);
+					}
 //				System.out.println(location);
-				
+				}
 			}
-
 		} catch (NamingException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
 
     
 }
